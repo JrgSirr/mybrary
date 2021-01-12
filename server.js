d1dfebabb79214e1bbdf32ff7b3212949a43c15f
luis.yepesm@arus.com.co <eraflav@gmail.com>
2021-01-12 10:41:52 -0500

    new models

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const express = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 
 app.set('view engine', 'ejs')
@@ -16,6 +18,7 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 
@@ -26,5 +29,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
